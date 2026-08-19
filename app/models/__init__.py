7b75c3e81800dd65aa61740e181f5802dcbf62fd
Thaeme <othaeme07@gmail.com>
2026-08-19 14:07:27 -0300

    Adiconar controle de clientes e venda

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,12 @@
 from app.models import categoria
 from app.models import produto
 from app.models import usuarios
-
+from app.models import movimentacao
+from app.models import cliente
+from app.models import venda
 # Gerar a migration
 
-# python -m alembic revision --autogenerate -m "Criar tabela categorias e produtos."
+# python -m alembic revision --autogenerate -m "Criar tabela cliente."
 
 # aplicar a migration
 # python -m alembic upgrade head
